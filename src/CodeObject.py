2ebd17234767ba066c53150d7af9d14c0a7cf693
Denis Proshutinskii <jl0pd@outlook.com>
2018-12-30 00:21:16 +0700

    Turing Machine now working
    Class Code renamed to Worker
    Class CodeStructure renamed to CodeString

--- src/CodeObject.py
+++ src/CodeObject.py
@@ -1,8 +1,11 @@
 import re
-from typing import List
+from functools import partial
+from typing import List, Dict, Set
+
+from src.Tape import Tape
 
 
-class CodeStructure(object):
+class CodeString(object):
     __slots__ = ("block_name", "change_from", "change_to", "move_to", "go_to", "__original")
 
     def __init__(self, code_str: str):
@@ -15,21 +18,66 @@ class CodeStructure(object):
             self.move_to = tokens[3]
             self.go_to = tokens[4]
         else:
-            raise SyntaxError(f'string "{code_str}" doesnt match pattern')
+            raise SyntaxError(f"""string "{code_str}" doesn't match pattern""")
 
     def __match_pattern(self):
-        code_re = re.compile(r"\w+ \w \w [lhrLHR] \w+")
+        block_name = r"\w+"
+        available_chars = r"""[0-9 _a-zA-Z*:;'"\|/,.?()]"""
+        directions = r"[lhrLHR]"
+        pattern = f"{block_name} {available_chars} {available_chars} {directions} {block_name}"
+        code_re = re.compile(pattern)
         result = re.findall(code_re, self.__original)
-        return bool(result)
+        return result
 
     def __str__(self):
         return self.__original
 
 
-class Code(object):
+def code_func(*, t: Tape, s: CodeString):
+    if s.change_to != '*':
+        t.active_node.symbol = s.change_to
+
+    if s.move_to in 'Rr':
+        t.move_right()
+    else:
+        t.move_left()
+    return s.go_to
+
+
+class Worker(object):
 
     def __init__(self, code_strings: List[str]):
-        self.__codes = [CodeStructure(_str) for _str in code_strings]
+        self.__blocks: List[CodeString] = [CodeString(_str) for _str in code_strings]
+        self.__in_names: Set[str] = set()
+        self.__out_names: Set[str] = set()
+        self.__compiled: Dict[str, Dict[str, type(lambda: ())]] = dict()
+
+    def __find_names(self):
+        for code_obj in self.__blocks:
+            self.__in_names.add(code_obj.block_name)
+            self.__out_names.add(code_obj.go_to)
+
+        if "start" not in self.__in_names or "end" not in self.__out_names:
+            raise SyntaxError("'start' and 'end' must be in code")
 
     def compile(self):
-        pass
+        self.__find_names()
+
+        for in_name in self.__in_names:
+            self.__compiled[in_name] = dict()
+
+        for code_obj in self.__blocks:
+            self.__compiled[code_obj.block_name][code_obj.change_from] = partial(code_func, s=code_obj)
+
+    def run(self, tape: Tape, out_file=None):
+        current_block = "start"
+        while current_block != "end":
+            block = self.__compiled[current_block]
+            current_symbol = tape.active_node.symbol
+            line_in_block_by_code = block[current_symbol]
+            current_block = line_in_block_by_code(t=tape)
+
+        if out_file:
+            out_file.write(str(tape))
+        else:
+            print(str(tape))
