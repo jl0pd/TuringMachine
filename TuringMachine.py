6f6c627af55a0c607516a15be0c70a68e4838c9b
Denis Proshutinskii <jl0pd@outlook.com>
2018-12-30 17:10:07 +0700

    Added program example

--- TuringMachine.py
+++ TuringMachine.py
@@ -1,12 +1,12 @@
 from src.CodeObject import Worker
 from src.Tape import Tape
-import src
-src.Tape.default_node_char = '_'
+
 if __name__ == '__main__':
-    file_path = "../t_p"
+    file_path = "example.TMProgram"
     file_content = open(file_path).read().split('\n')
-    out_file = open("../tp_out.txt", "w")
+    out_file = open("tp_out.txt", "w")
     worker = Worker(file_content)
-    tape = Tape("hi there!")
+    tape = Tape("_____h")
+    print(str(tape))
     worker.compile()
     worker.run(tape, out_file=out_file)
